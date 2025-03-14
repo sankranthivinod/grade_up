ef1f651779e27559fd6154a29707ad2a4b5b6891
sankranthivinod <sankranthivinod@gmail.com>
2025-03-14 12:05:42 -0700

    initial commit

---
+++ lib/onboarding/splashscreen_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashscreenPage extends StatelessWidget {
+  const SplashscreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
